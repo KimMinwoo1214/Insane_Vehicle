6a2cd0b2ad62a7420ccee595d5526f0653e781d8
KimMinwoo1214 <werkm1214@hanyang.ac.kr>
2025-02-12 14:47:54 +0900

    teleop ROS2

---
+++ Arduino_Control/keybd/ROS2_con/ROS2_con.ino
@@ -0,0 +1,46 @@
+#include "CytronMotorDriver.h"
+
+// 핀 설정
+const int pwmPin_r = 3;
+const int dirPin_r = 4;
+const int pwmPin_l = 5;
+const int dirPin_l = 6;
+const int pwmPin_s = 8;
+const int dirPin_s = 9;
+
+CytronMD motor1(PWM_DIR, pwmPin_r, dirPin_r);
+CytronMD motor2(PWM_DIR, pwmPin_l, dirPin_l);
+CytronMD motor3(PWM_DIR, pwmPin_s, dirPin_s);
+
+int speed = 0;  // 속도 변수
+
+void controlDriveMotor(String command) {
+  command.trim();  // 개행 문자 제거
+
+  if (command.startsWith("forward:")) {
+    speed = command.substring(8).toInt();  // 속도 값 파싱
+    motor1.setSpeed(speed);
+    motor2.setSpeed(speed);
+  } else if (command == "stop") {
+    speed = 0;
+    motor1.setSpeed(0);
+    motor2.setSpeed(0);
+  } else if (command == "ccw") {
+    motor3.setSpeed(-100);
+  } else if (command == "cw") {
+    motor3.setSpeed(100);
+  } else {
+    motor3.setSpeed(0);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');  // '\n'까지 문자열 읽기
+    controlDriveMotor(command);
+  }
+}
---
+++ Arduino_Control/ros2_con/src/mega_con/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'mega_con'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kmw',
+    maintainer_email='werkm1214@hanyang.ac.kr',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    entry_points={
+        'console_scripts': [
+        'tel = mega_con.tel_motor:main',
+        'con = mega_con.motor_con:main',
+        ],
+    },
+)
