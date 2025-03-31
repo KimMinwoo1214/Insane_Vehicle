9645ab3e4206bb788edf2d00d1e485272f435803
antel <ati0423@hanyang.ac.kr>
2025-03-31 17:27:38 +0900

    Change port to video1

--- camera_ws/src/ros2_cv_cam/ros2_cv_cam/cam_pub.py
+++ camera_ws/src/ros2_cv_cam/ros2_cv_cam/cam_pub.py
@@ -58,7 +58,7 @@ class ImagePublisher(Node):
          
     # Create a VideoCapture object
     # The argument '0' gets the default webcam.
-    self.cap = cv2.VideoCapture(0)
+    self.cap = cv2.VideoCapture(1)
          
     # Used to convert between ROS and OpenCV images
     self.br = CvBridge()
