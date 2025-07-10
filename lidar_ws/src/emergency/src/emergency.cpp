31a0a3ef23095bea487fb501e2870e3b18cfa03d
KimMinwoo1214 <werkm1214@hanyang.ac.kr>
2025-07-11 02:47:03 +0900

    Emergency

--- lidar_ws/src/emergency/src/emergency.cpp
+++ lidar_ws/src/emergency/src/emergency.cpp
@@ -24,9 +24,9 @@ public:
     declare_parameter<std::string>("cloud_topic", "/clustered_points");
     declare_parameter<std::string>("emergency_topic", "/emergency");
     declare_parameter<float>("roi_min_x", 0.3f);
-    declare_parameter<float>("roi_max_x",  3.0f);
-    declare_parameter<float>("roi_min_y", -0.9f); //실전 상황에서 동적 장애물 등장 방향보고 비대칭적 ROI 적용하기
-    declare_parameter<float>("roi_max_y",  0.9f);
+    declare_parameter<float>("roi_max_x",  1.0f);
+    declare_parameter<float>("roi_min_y", -0.3f); //실전 상황에서 동적 장애물 등장 방향보고 비대칭적 ROI 적용하기
+    declare_parameter<float>("roi_max_y",  0.3f);
     declare_parameter<double>("min_dist", 1.0);
 
     get_parameter("cloud_topic", cloud_topic_);
